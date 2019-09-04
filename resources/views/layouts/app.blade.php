dee8dea970789346092b25f96594cc686a70514b
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2019-09-04 17:49:20 +0000

    A user can filter threads by popularity

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,23 @@
             'csrfToken' => csrf_token(),
         ]) !!};
     </script>
+
+    <style>
+        body {
+            padding-bottom: 100px;
+        }
+
+        .level {
+            display: flex;
+            align-items: center;
+        }
+
+        .flex {
+            flex: 1;
+        }
+    </style>
 </head>
-<body style="padding-bottom: 100px;">
+<body>
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
@@ -51,6 +66,10 @@
                                 @if (auth()->check())
                                     <li><a href="/threads?by={{ auth()->user()->name }}">My Threads</a></li>
                                 @endif
+
+                                <li>
+                                    <a href="/threads?popular=1">Popular Threads</a>
+                                </li>
                             </ul>
                         </li>
 
