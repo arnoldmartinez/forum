b88a464b3aebf7bf1bec08a016ade2f83b70cef2
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2019-09-09 16:08:12 +0000

    How to construct an activity feed with TDD: part 2

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -6,11 +6,28 @@ use App\User;
 
 class ProfilesController extends Controller
 {
+    /**
+     * Show the user's profile.
+     *
+     * @param  User  $user
+     * @return \Response
+     */
     public function show(User $user)
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'threads' => $user->threads()->paginate(30)
+            'activities' => $this->getActivity($user)
         ]);
+    }
+
+    /**
+     * @param User $user
+     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Relations\HasMany[]|\Illuminate\Database\Query\Builder[]|\Illuminate\Support\Collection
+     */
+    protected function getActivity(User $user)
+    {
+        return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
+            return $activity->created_at->format('Y-m-d');
+        });
     }
 }
