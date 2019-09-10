251766ca51a0ebe7ea9b720a8cd1a0433860253d
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2019-09-10 16:06:53 +0000

    Extracting controller queries to the model

--- app/Activity.php
+++ app/Activity.php
@@ -6,10 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
+    /**
+     * Don't auto-play mass assigment protection.
+     *
+     * @var array
+     */
     protected $guarded = [];
 
+
+    /**
+     * Fetch the associated subject for the activity.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
     public function subject()
     {
         return $this->morphTo();
+    }
+
+    public static function feed($user, $take = 50)
+    {
+        return static::where('user_id', $user->id)
+            ->latest()
+            ->with('subject')
+            ->take($take)
+            ->get()
+            ->groupBy(function ($activity) {
+                return $activity->created_at->format('Y-m-d');
+            });
     }
 }
