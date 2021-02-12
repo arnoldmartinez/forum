486325133f6f375b9e04b057d86e8e08372570cf
Arnold Martinez <arnoldmartinez@criptext.com>
2021-02-11 21:55:06 -0600

    A user may not reply more than than once per minute
    Episode 54

--- app/Policies/ReplyPolicy.php
+++ app/Policies/ReplyPolicy.php
@@ -10,8 +10,24 @@ class ReplyPolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * Determine if the authenticated user has permission to update a reply.
+     *
+     * @param  User  $user
+     * @param  Reply $reply
+     * @return bool
+     */
     public function update(User $user, Reply $reply)
     {
         return $reply->user_id == $user->id;
+     }
+
+     public function create(User $user)
+     {
+         if (! $lastReply = $user->fresh()->lastReply) {
+             return true;
+         }
+
+         return ! $lastReply->wasJustPublished();
      }
 }
