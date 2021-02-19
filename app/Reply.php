82516c26d97e3d9e6e65bc08c18d645169957158
Arnold Martinez <arnoldmartinez@criptext.com>
2021-02-18 22:19:44 -0600

    Wrap usernames within anchor tags
    Episode 59

--- app/Reply.php
+++ app/Reply.php
@@ -76,7 +76,7 @@ class Reply extends Model
     public function mentionedUsers()
     {
         // Inspect the body of the reply for username mentions (regexr.com)
-        preg_match_all('/\@([^\s\.]+)/', $this->body, $matches);
+        preg_match_all('/@([\w\-]+)/', $this->body, $matches);
 
         return $matches[1];
     }
@@ -89,5 +89,10 @@ class Reply extends Model
     public function path()
     {
         return $this->thread->path() . "#reply-{$this->id}";
+    }
+
+    public function setBodyAttribute($body)
+    {
+        $this->attributes['body'] = preg_replace('/@([\w\-]+)/', '<a href="/profiles/$1">$0</a>', $body);
     }
 }
