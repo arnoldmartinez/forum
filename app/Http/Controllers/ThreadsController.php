05332bb731ca4e6f28c0a08ffe14ee74e0cbff0c
Arnold Martinez <arnoldmartinez@criptext.com>
2021-02-23 22:10:54 -0600

    Basic view tweaks
    Episode 62

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -144,6 +144,6 @@ class ThreadsController extends Controller
             $threads->where('channel_id', $channel->id);
         }
 
-        return $threads->get();
+        return $threads->paginate(25);
     }
 }
