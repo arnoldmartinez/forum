93971db0f474984b800b45a138104a6208ff33dc
Arnold Martinez <arnoldmartinez@criptext.com>
2021-03-09 19:43:44 -0600

    Thread views: design #2 - extract class
    Episode 69

--- app/Thread.php
+++ app/Thread.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    use RecordsActivity, RecordsVisits;
+    use RecordsActivity;
 
     /**
      * Don't auto-apply mass assignment protection.
@@ -168,4 +168,8 @@ class Thread extends Model
         return $this->updated_at > cache($key);
     }
 
+    public function visits()
+    {
+        return new Visits($this);
+    }
 }
