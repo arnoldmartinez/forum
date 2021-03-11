21cb93c82fc20622b4322b3fc0eea4dfeb96002c
Arnold Martinez <arnoldmartinez@criptext.com>
2021-03-11 07:43:48 -0600

    Thread views: design #3 - KISS
    php artisan migrate:refresh
    php artisan tinker
    factory('App\Thread', 30)->create()

--- resources/views/threads/_list.blade.php
+++ resources/views/threads/_list.blade.php
@@ -29,7 +29,7 @@
             </div>
 
             <div class="panel-footer">
-                {{ $thread->visits()->count() }} Visits
+                {{ $thread->visits }} Visits
             </div>
         </div>
     @empty
