d049201a2f95666befa9164887a625d5977fb8a8
Arnold Martinez <arnoldmartinez@criptext.com>
2021-04-01 08:25:59 -0600

    Users must confirm their email address: #3 cleanup
    Episode 73

--- app/Http/Controllers/Api/RegisterConfirmationController.php
+++ app/Http/Controllers/Api/RegisterConfirmationController.php
@@ -11,11 +11,16 @@ class RegisterConfirmationController extends Controller
 {
     public function index()
     {
-        User::where('confirmation_token', request('token'))
-            ->firstOrFail()
-            ->confirm();
+        try {
+            User::where('confirmation_token', request('token'))
+                ->firstOrFail()
+                ->confirm();
+        } catch (\Exception $e) {
+            return redirect(route('threads'))
+                ->with('flash', 'Unknown token.');
+        }
 
-        return redirect('/threads')
+        return redirect(route('threads'))
             ->with('flash', 'You account is now confirmed! You may post to the forum.');
     }
 }
--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -18,7 +18,12 @@ class RegistrationTest extends TestCase
     {
         Mail::fake();
 
-        event(new Registered(create('App\User')));
+        $this->post(route('register'), [
+            'name' => 'John',
+            'email' => 'john@example.com',
+            'password' => 'foobar',
+            'password_confirmation' => 'foobar'
+        ]);
 
         Mail::assertSent(PleaseConfirmYourEmail::class );
     }
@@ -26,7 +31,9 @@ class RegistrationTest extends TestCase
     /** @test */
     function user_can_fully_confirm_their_email_addresses()
     {
-        $this->post('/register', [
+        Mail::fake();
+
+        $this->post(route('register'), [
             'name' => 'John',
             'email' => 'john@example.com',
             'password' => 'foobar',
@@ -38,11 +45,17 @@ class RegistrationTest extends TestCase
         $this->assertFalse($user->confirmed);
         $this->assertNotNull($user->confirmation_token);
 
-        // Let the user confirm their account.
-        $response = $this->get('/register/confirm?token' . $user->confirmation_token);
+        $this->get(route('register.confirm', ['token' => $user->confirmation_token]))
+            ->assertRedirect(route('threads'));
 
         $this->assertTrue($user->fresh()->confirmed);
+    }
 
-        $response->assertRedirect('/threads');
+    /** @test */
+    function confirming_an_invalid_token()
+    {
+        $this->get(route('register.confirm', ['token' => 'invalid']))
+            ->assertRedirect(route('threads'))
+            ->assertSessionHas('flash', 'Unknown token.');
     }
 }
