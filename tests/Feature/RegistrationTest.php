bbedde2413beae10bf1265c2f96c8fb3f2dae5aa
Arnold Martinez <arnoldmartinez@criptext.com>
2021-04-02 10:16:20 -0600

    Email confirmation loose ends
    Episode 74

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -8,6 +8,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
+use function foo\func;
 
 class RegistrationTest extends TestCase
 {
@@ -48,7 +49,10 @@ class RegistrationTest extends TestCase
         $this->get(route('register.confirm', ['token' => $user->confirmation_token]))
             ->assertRedirect(route('threads'));
 
-        $this->assertTrue($user->fresh()->confirmed);
+        tap($user->fresh(), function ($user) {
+            $this->assertTrue($user->confirmed);
+            $this->assertNull($user->confirmation_token);
+        });
     }
 
     /** @test */
